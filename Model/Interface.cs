8a9269ef4a14669a70ae5a4db48fd4ac110c5114
Bella Fickling <igfickling@gmail.com>
2025-06-25 22:17:26 +1200

    IPizza interface created

---
+++ Model/Interface.cs
@@ -0,0 +1,14 @@
+﻿namespace DSD601_Pizza_App.Model
+{
+    public interface IPizza
+    {
+        string Flavour { get; set; }
+        string Description { get; set; }
+        string PizzaChef { get; set; }
+        string ImageUrl { get; set; }
+        List<string> Ingredients { get; set; }
+        bool IsGlutenFree { get; set; }
+        bool IsVegetarian { get; }
+        void UpdateIsVegetarian();
+    }
+}
